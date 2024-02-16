b45bf6491031b3bdb3e101251f8ac2454d85fef0
Juan-789 <juan.marulanda@stu.ocsb.ca>
2024-02-16 02:42:57 -0500

    commiting

---
+++ docs/source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import pathlib, sys
+sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+
+project = 'Ground Station Documentation'
+copyright = '2024, ground station subteam'
+author = 'ground station subteam'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.doctest', 'sphinx.ext.autodoc', 'sphinx.ext.autosummary']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "groundwork"
+html_static_path = ['_static']
+html_css_files = ['custom.css']
